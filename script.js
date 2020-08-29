46f46aa235454564e17a982af834266ed6a349b4
Jonathan <jonathancamiling@gmail.com>
2020-08-29 08:08:36 -0700

    added addEventlistener for username value

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+//Show input error message
+function showError(input, message) {
+    
+}
+
+//Event Listeners
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (username.value === '') {
+        showError(username, 'Username is requred');
+    } else {
+        showSuccess(username);
+    }
+})
